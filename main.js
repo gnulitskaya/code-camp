eb15935c81d88b8a6793e33971bba10982e6a796
gnulitskaya <webgnule@mail.ru>
2023-04-04 23:13:09 +0300

    sumFibs resolved

--- main.js
+++ main.js
@@ -55,4 +55,30 @@ function convertHTML(str) {
   return temp;
 }
 
-convertHTML("Dolce & Gabbana");
+convertHTML(temp);
+
+// Sum All Odd Fibonacci Numbers
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+// The first two numbers in the Fibonacci sequence are 0 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first seven numbers of the Fibonacci sequence are 0, 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+// sumFibs(75024) should return 60696.
+
+function sumFibs(num) {
+  let activeNum = 1;
+  let prevNum = 0;
+  let summ = 0;
+
+   while(activeNum <= num) {
+     if (activeNum % 2 !== 0) {
+       summ += activeNum;
+     }
+     activeNum += prevNum;
+     prevNum = activeNum - prevNum;
+   }
+   
+  return summ;
+}
+
+sumFibs(4);

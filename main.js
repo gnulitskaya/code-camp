cda985d6ffceb6a459e76029640cb84d75ab699b
gnulitskaya <webgnule@mail.ru>
2023-04-25 11:23:19 +0300

    resolve drop it

--- main.js
+++ main.js
@@ -113,3 +113,63 @@ function sumPrimes(num) {
 }
 
 sumPrimes(10);
+
+// Smallest Common Multiple
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+// The range will be an array of two numbers that will not necessarily be in numerical order.
+
+// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+// smallestCommons([1, 5]) should return 60.
+
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const numberDivisors = max - min + 1;
+  // Largest possible value for SCM
+  let upperBound = 1;
+  for (let i = min; i <= max; i++) {
+    upperBound *= i;
+  }
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    let divisorCount = 0;
+    for (let i = min; i <= max; i++) {
+      // Count divisors
+      if (multiple % i === 0) {
+        divisorCount += 1;
+      }
+    }
+    if (divisorCount === numberDivisors) {
+      return multiple;
+    }
+  }
+}
+
+smallestCommons([1, 5]);
+
+
+// Drop it
+// Given the array arr, iterate through and remove each element starting from 
+// the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+// dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+
+function dropElements(arr, func) {
+  let newArray = [];
+  let firstIndex = -1;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      firstIndex = i;
+      break;
+    }
+  }
+
+  newArray = firstIndex == -1 ? [] : arr.slice(firstIndex);
+  return newArray;
+}
+
+dropElements([1, 2, 3], function(n) {return n > 0;})

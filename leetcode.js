b5721d37d159a42c423d66ea3b6d0bea38810ccb
gnulitskaya <webgnule@mail.ru>
2023-06-05 19:36:19 +0300

    task

--- leetcode.js
+++ leetcode.js
@@ -157,3 +157,28 @@ const counter = createCounter(5)
 counter.increment(); // 6
 counter.reset(); // 5
 counter.decrement(); // 4
+
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+var once = function(fn) {
+    let oneCall = false;
+
+    return function(...args){
+        if(!oneCall) {
+            oneCall = true;
+
+            return fn(...args);
+        } else {
+            return undefined;
+        }
+    }
+};
+
+let fnt = (a,b,c) => (a + b + c)
+let onceFn = once(fnt)
+
+onceFn(1,2,3); // 6
+onceFn(2,3,6); // returns undefined without calling fn

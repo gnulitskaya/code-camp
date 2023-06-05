57f16094ee205d2007e8de6df303055d829cf800
gnulitskaya <webgnule@mail.ru>
2023-06-05 18:34:49 +0300

    resolve

--- leetcode.js
+++ leetcode.js
@@ -93,3 +93,67 @@ arr = [1,2,3];
 fn = function plusone(n) { return n + 1; };
 
 const newArray = map(arr, plusone); // [2,3,4]
+
+
+// Given an asyncronous function fn and a time t in milliseconds, return a new time limited version of the input function.
+// A time limited function is a function that is identical to the original unless it takes longer than t milliseconds to fullfill. In that case, it will reject with "Time Limit Exceeded".  Note that it should reject with a string, not an Error.
+
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        console.log(...args, fn, t);
+        const timeLimitFunc = fn(...args);
+
+        const timeout = new Promise((_, reject) => {
+            setTimeout(() => {reject('Time Limit Exceeded')}, t)
+        })
+
+        return Promise.race([timeLimitFunc, timeout]);
+    }
+};
+
+const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 250);
+limited(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
+
+
+// Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+// A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+// If the length of the array is 0, it should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+
+var reduce = function(nums, fn, init) {
+    let finalSum = init;
+
+    for(let i = 0; i < nums.length; i++) {
+        finalSum = fn(finalSum, nums[i]);
+    }
+
+    console.log(finalSum);
+    return finalSum;
+};
+
+
+reduce([1,2,3,4], function sum(accum, curr) { return accum + curr; }, 0);
+
+// Write a function createCounter. It should accept an initial integer init. 
+// It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
+var createCounter = function(init) {
+    function increment() {
+        console.log('sxasx');
+    }
+
+};
+
+const counter = createCounter(5)
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
